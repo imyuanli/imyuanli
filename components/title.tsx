d6fe224ab7f0752b6784245d4e4b94c4a837dab0
yuanli <2865437316@qq.com>
2023-04-14 15:57:03 +0800

    refactor: title组件封装

--- components/title.tsx
+++ components/title.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import {Divider, Input} from "antd";
-import {SearchOutlined} from "@ant-design/icons";
+import {Divider} from "antd";
 
 interface props {
-    value: any
+    value: any,
+    children?:any
 }
 
-const Title: React.FunctionComponent<props> = ({value}) => {
+const Title: React.FunctionComponent<props> = ({value,children}) => {
     return (
         <>
-            <div className={'py-9'}>
+            <div>
                 <div className={'grid grid-cols-4 items-center mb-3'}>
                     <div className={'col-span-2 text-4xl font-extrabold leading-9 tracking-tight text-gray-900'}>
                         {value}
                     </div>
                     <div className={'col-span-2'}>
-                        <Input
-                            placeholder="Basic usage"
-                            size={'large'}
-                            suffix={<SearchOutlined/>}
-                        />
+                        {children}
                     </div>
                 </div>
                 <Divider/>
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,6 +1,5 @@
-import {Divider} from "antd";
-import {EyeOutlined, MessageOutlined} from "@ant-design/icons";
-import Link from "next/link";
+import {Divider, Input} from "antd";
+import {SearchOutlined} from "@ant-design/icons";
 import Title from "@/components/title";
 import {test_article} from "@/utils";
 import ArticleCard from "@/components/article-card";
@@ -9,7 +8,13 @@ import React from "react";
 const Blog = () => {
     return (
         <div>
-            <Title value={'全部文章'}/>
+            <Title value={'全部文章'}>
+                <Input
+                    placeholder="Basic usage"
+                    size={'large'}
+                    suffix={<SearchOutlined/>}
+                />
+            </Title>
             <div className={'space-y-20'}>
                 <div className={'grid grid-cols-1  gap-6'}>
                     {test_article.map((item, index) => {
