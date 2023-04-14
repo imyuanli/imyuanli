d4e47e09be0b53b41bd8766bc4c5300d96502f36
yuanli <2865437316@qq.com>
2023-04-14 15:57:25 +0800

    feat: 新增作品页

---
+++ pages/work.tsx
@@ -0,0 +1,22 @@
+import Title from "@/components/title";
+import {DEFAULT_WORK_LIST} from "@/utils";
+import React from "react";
+import WorkCard from "@/components/work-card";
+
+const Blog = () => {
+    return (
+        <div>
+            <Title value={'全部作品'}/>
+            <div className={'grid grid-cols-1 md:grid-cols-3 gap-3'}>
+                {DEFAULT_WORK_LIST.map((item, index) => {
+                    return (
+                        <WorkCard key={index} workItem={item}/>
+                    )
+                })}
+            </div>
+        </div>
+    );
+}
+
+
+export default Blog
