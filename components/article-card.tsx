688f41bc8942bf3a33dca72556145efb6e9a5460
yuanli <2865437316@qq.com>
2023-05-06 17:57:21 +0800

    fix: 类型错误

--- components/article-card.tsx
+++ components/article-card.tsx
@@ -7,15 +7,15 @@ import {Card} from "antd";
 interface props {
     article: any,
     isBlog?: Boolean,
-    inputValue:string
+    inputValue?: any
 }
 
-const ArticleCard: React.FunctionComponent<props> = ({article, isBlog,inputValue}) => {
+const ArticleCard: React.FunctionComponent<props> = ({article, isBlog, inputValue}) => {
     const {article_id, title, describe, classify_value, create_time, view_count} = article
 
     const textHighLight = (value: string) => {
         if (value.indexOf(inputValue) !== -1 && inputValue !== '') {
-            return value.replace(inputValue, `<font color={metadata.primaryColor}>${inputValue}</font>`)
+            return value.replace(inputValue, `<font color={'green'}>${inputValue}</font>`)
         }
         return value
     }
@@ -29,10 +29,10 @@ const ArticleCard: React.FunctionComponent<props> = ({article, isBlog,inputValue
                         href={`/blog/${article_id}`}
                         className={'text-xl font-semibold mb-3 title-underline line-clamp-1'}
                     >
-                        <span dangerouslySetInnerHTML={{__html: textHighLight(title)}} />
+                        <span dangerouslySetInnerHTML={{__html: textHighLight(title)}}/>
                     </Link>
                     <div className={'text-gray-500  line-clamp-3 mb-3'}>
-                        <span dangerouslySetInnerHTML={{__html: textHighLight(describe)}} />
+                        <span dangerouslySetInnerHTML={{__html: textHighLight(describe)}}/>
                     </div>
                     <div className={'mt-auto w-full flex justify-end'}>
                         <MyLink href={`/blog/${article_id}`} text={'继续阅读'}/>
