08e2718ade81edcc14ea4f8b6f679301c836dc9b
yuanli <2865437316@qq.com>
2023-05-06 17:41:52 +0800

    feat: 文章分类跳转

--- components/time-classify.tsx
+++ components/time-classify.tsx
@@ -1,14 +1,20 @@
 import dayjs from "dayjs";
 import MyLink from "@/components/my-link";
 import React from "react";
+import {useAtomValue} from "jotai";
+import {classifyAtom} from "@/store";
 
 interface props {
     create_time: any,
     classify_value: any,
-    view_count:any
+    view_count: any
 }
 
-const TimeClassify: React.FunctionComponent<props> = ({create_time, classify_value,view_count}) => {
+const TimeClassify: React.FunctionComponent<props> = ({create_time, classify_value, view_count}) => {
+
+    const classifyList: any = useAtomValue(classifyAtom);
+    //获取当前名称
+    const getCurName = (value: string) => classifyList && classifyList.find((item: any) => item.classify_value == value)?.classify_label
     return (
         <div className={'flex items-baseline text-sm text-gray-500 space-x-1'}>
             <div>{dayjs(create_time).format('YYYY-MM-DD')}</div>
@@ -17,7 +23,7 @@ const TimeClassify: React.FunctionComponent<props> = ({create_time, classify_val
                 阅读 {view_count}
             </div>
             <div>{` • `}</div>
-            <div>{classify_value}</div>
+            <MyLink href={`/blog?classify=${classify_value}`} text={getCurName(classify_value)}/>
         </div>
     );
 }
